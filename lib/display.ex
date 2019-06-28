acbc29acba2cde40812e802cf0494230fc1f3409
Tola's Terminal <sohbaker@gmail.com>
2019-06-28 09:33:02 +0100

    prompts player to make a move

--- lib/display.ex
+++ lib/display.ex
@@ -2,4 +2,8 @@ defmodule Display do
   def greeting do 
     "Welcome to Tic Tac Toe!"
   end
+  
+  def prompt_player(mark) do
+    "Make a move: #{mark}" 
+  end
 end
--- test/display_test.exs
+++ test/display_test.exs
@@ -1,8 +1,14 @@
 defmodule DisplayTest do
   use ExUnit.Case
   doctest Display 
+  import Board
 
   test "prints a greeting" do
     assert Display.greeting() == "Welcome to Tic Tac Toe!"
+  end
+
+  test "prompts a player to make a move" do
+    mark = "X"
+    assert Display.prompt_player(mark) == "Make a move: X" 
   end
 end
