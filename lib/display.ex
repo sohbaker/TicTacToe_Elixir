7717dc7fe97729d20e9ca8ea0b23731b46c9e064
Tola's Terminal <sohbaker@gmail.com>
2019-07-01 11:10:23 +0100

    add line breaks. privatise functions

--- lib/display.ex
+++ lib/display.ex
@@ -1,14 +1,14 @@
 defmodule Display do 
   def greeting do 
-    "Welcome to Tic Tac Toe!"
+    "Welcome to Tic Tac Toe!\n"
   end
   
   def prompt_player(mark) do
-    "Make a move: #{mark}" 
+    "Make a move: #{mark}\n" 
   end
 
   def notify_invalid do 
-    "Invalid move. Please try again"
+    "Invalid move. Please try again\n"
   end
 
   def show_board(grid) do
@@ -16,33 +16,33 @@ defmodule Display do
     |> List.flatten
     |> Enum.join
   end
-
-  def format_board(grid) do  
-    line_separator = String.duplicate("-", length(grid))
-    Enum.map(grid, fn x -> "#{x} | " end)
-    |> clean_up_board(3, line_separator)
-  end
-
-  def clean_up_board(grid, n, line_separator) when n > 0 do
-    first_two_items  = Enum.take(grid, n - 1)   
-    last_item  = Enum.map(Enum.take(grid, -1), fn x -> String.replace(x, " | ", "\n") end)
-    grid = Enum.drop(grid, n - 1)
-    grid = Enum.drop(grid, -1)     
-    body = Enum.map_every(grid, n, fn x -> String.replace(x, " | ", "\n#{line_separator}\n") end)
-    first_two_items ++ body ++ last_item  
-  end
   
   def announce_win(mark) do 
-    "Player #{mark} wins!"
+    "Player #{mark} wins!\n"
   end
   
   def announce_tie do
-   "It's a tie!"
+   "It's a tie!\n"
   end
 
   def get_input(prompt) do 
     IO.gets(prompt)
     |> String.trim
     |> String.to_integer  
+  end
+
+  defp format_board(grid) do  
+    line_separator = String.duplicate("-", length(grid))
+    Enum.map(grid, fn x -> "#{x} | " end)
+    |> prettify_board(3, line_separator)
+  end
+
+  defp prettify_board(grid, n, line_separator) when n > 0 do
+    first_two_items  = Enum.take(grid, n - 1)   
+    last_item  = Enum.map(Enum.take(grid, -1), fn x -> String.replace(x, " | ", "\n") end)
+    grid = Enum.drop(grid, n - 1)
+    grid = Enum.drop(grid, -1)     
+    body = Enum.map_every(grid, n, fn x -> String.replace(x, " | ", "\n#{line_separator}\n") end)
+    first_two_items ++ body ++ last_item  
   end
 end 
--- test/display_test.exs
+++ test/display_test.exs
@@ -4,21 +4,20 @@ defmodule DisplayTest do
   import ExUnit.CaptureIO
 
   test "prints a greeting" do
-    assert Display.greeting() == "Welcome to Tic Tac Toe!"
+    assert Display.greeting() == "Welcome to Tic Tac Toe!\n"
   end
 
   test "prompts a player to make a move" do
     mark = "X"
-    assert Display.prompt_player(mark) == "Make a move: X" 
+    assert Display.prompt_player(mark) == "Make a move: X\n" 
   end
 
   test "notifies a player of an invalid move" do 
-    assert Display.notify_invalid() == "Invalid move. Please try again"
+    assert Display.notify_invalid() == "Invalid move. Please try again\n"
   end
 
   test "shows the board" do
     grid = Board.grid()
-    IO.puts(Display.show_board(grid))
     assert Display.show_board(grid) == """
 1 | 2 | 3
 ---------
@@ -29,11 +28,11 @@ defmodule DisplayTest do
   end
 
   test "announces a winner" do 
-    assert Display.announce_win("X") == "Player X wins!"
+    assert Display.announce_win("X") == "Player X wins!\n"
   end
 
   test "announces a tie" do 
-    assert Display.announce_tie == "It's a tie!"
+    assert Display.announce_tie == "It's a tie!\n"
   end
 
   test "takes an input from a user" do 
