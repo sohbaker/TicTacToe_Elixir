758ebd2053a63ddb8c17df97b613539c6f9b2757
Tola's Terminal <sohbaker@gmail.com>
2019-07-02 16:38:22 +0100

    player one makes a move

--- lib/game.ex
+++ lib/game.ex
@@ -1,15 +1,48 @@
 defmodule Game do 
-  defstruct [:player_one, :player_two, :current_player]
+  defstruct [:board, :player_one, :player_two, :current_player]
 
-  def new(player_one_mark, player_two_mark ) do
+  def new(player_one, player_two) do
     game = %Game{
-      player_one: %Human{mark: player_one_mark},
-      player_two: %Human{mark: player_two_mark},
+      player_one: player_one,
+      player_two: player_two,
+      board: Board.grid()
     }
     %Game{game| current_player: game.player_one}
   end
-  
+
+  def start(%Game{board: board, current_player: player} = game) do
+    move = Player.get_move(player)
+    new_board = Board.mark_board(board, move, player.mark)
+
+    new_player = toggle_player(game, player)
+    %Game{ game | board: new_board, current_player: new_player}
+  end
+
+  def toggle_player(game, player) do
+    if player.mark == game.player_one.mark do
+      game.player_two
+    else
+      game.player_one
+    end
+  end
+
   def over?(game, board) do 
     Board.tie?(board, game.player_one.mark, game.player_two.mark) || Board.win?(board, game.player_one.mark) || Board.win?(board, game.player_two.mark)
+  end
+end
+
+defprotocol Player do 
+  def get_move(player)
+end
+
+defimpl Player, for: Human do
+  def get_move(_player) do 
+    "hello"
+  end
+end
+
+defimpl Player, for: StubPlayer do
+  def get_move(_player) do
+    1
   end
 end
--- test/game_test.exs
+++ test/game_test.exs
@@ -3,11 +3,23 @@ defmodule GameTest do
   doctest Game
 
   test "knows that the game can continue" do
-    player_one = "X"
-    player_two = "O"
+    player_one = %StubPlayer{mark: "X"}
+    player_two = %StubPlayer{mark: "O"}
     game = Game.new(player_one, player_two)
     board = Board.grid()
 
     assert Game.over?(game, board) == false
-  end     
-end 
+  end
+
+  test "player one makes a move" do
+    player_one = %StubPlayer{mark: "X"}
+    player_two = %StubPlayer{mark: "O"}
+
+    %Game{board: b, current_player: c} =
+      Game.new(player_one, player_two)
+      |> Game.start()
+
+    assert ["X", "2", "3", "4", "5", "6", "7", "8", "9"] == b
+    assert %StubPlayer{mark: "O"} == c
+  end
+end
