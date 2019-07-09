8dc824826854ffe49148ed8c438789054c25d4d5
Tola's Terminal <sohbaker@gmail.com>
2019-07-09 14:29:55 +0100

    amend show_board function. use Elixir defined functions

--- lib/display.ex
+++ lib/display.ex
@@ -12,9 +12,7 @@ defmodule Display do
   end
 
   def show_board(grid) do
-    format_board(grid)
-    |> List.flatten()
-    |> Enum.join()
+    Display.print_to_screen(format_board(grid))
   end
 
   def announce_win(mark) do
@@ -36,17 +34,11 @@ defmodule Display do
 
   defp format_board(grid) do
     line_separator = String.duplicate("-", length(grid))
-
-    Enum.map(grid, fn x -> "#{x} | " end)
-    |> prettify_board(3, line_separator)
-  end
-
-  defp prettify_board(grid, n, line_separator) when n > 0 do
-    first_two_items = Enum.take(grid, n - 1)
-    last_item = Enum.map(Enum.take(grid, -1), fn x -> String.replace(x, " | ", "\n") end)
-    grid = Enum.drop(grid, n - 1)
-    grid = Enum.drop(grid, -1)
-    body = Enum.map_every(grid, n, fn x -> String.replace(x, " | ", "\n#{line_separator}\n") end)
-    first_two_items ++ body ++ last_item
+    rows = Enum.chunk_every(grid, 3)
+    for row <- rows do
+      Enum.join(row, " | ")
+      |> Kernel.<>("\n")
+    end
+    |> Enum.join("#{line_separator}\n")
   end
 end
