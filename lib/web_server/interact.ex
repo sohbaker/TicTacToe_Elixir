9cf4ddb89890143c80ecead50c193f21558caa0a
Tola's Terminal <sohbaker@gmail.com>
2019-09-20 16:17:48 +0100

    wip

--- lib/web_server/interact.ex
+++ lib/web_server/interact.ex
@@ -1,2 +1,17 @@
 defmodule WebServer.Interact do
+  #def play(game) do
+  #  if Board.over?(game.board) do
+  #    game_over_response(game)
+  #  else
+  #    post_new_move(game)
+  #    |> play()
+  #  end
+  #end
+
+  #def post_new_move(game) do
+  #  Game.take_turn(move, game)
+  #end
+
+  #def game_over_response(game) do
+  #end
 end
