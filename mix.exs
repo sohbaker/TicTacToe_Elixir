0920209c9c43306e966fcbc16294a797f9b9f775
Siobhan Baker <sohbaker@gmail.com>
2024-03-25 01:59:13 +0000

    update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TicTacToe.MixProject do
     [
       app: :tictactoe,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       escript: escript(),
       deps: deps()
